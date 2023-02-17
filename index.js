c39ffaa028644a1085cc9b096ee33ddb698d2fa0
sarojrani <sarojkumarirani345@gmail.com>
2023-02-17 12:57:57 +0530

    db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const cors=require("cors")
+const user = require("./db/user");
 const app = express();
+require("./db/config")
+require('./db/user')
+app.use(express.json())
+app.use(cors())
 const dotnev = require("dotenv").config()
 app.get("/",(req,res)=>{
     res.send("app is working")
+})
+app.post('/register',async(req,res)=>{
+    let data=req.body;
+    let newdata=await user.create(data)
+    return res.status(201).send({status:true,data:newdata})
 })
 
 app.listen(process.env.PORT,(err)=>{
